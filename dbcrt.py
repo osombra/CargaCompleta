ac96a542568136817aabe36f23275d650147d082
osombra <72713329+osombra@users.noreply.github.com>
2024-08-18 20:38:01 -0300

    Update dbcrt.py

--- dbcrt.py
+++ dbcrt.py
@@ -1,10 +1,10 @@
 import pyodbc
 
 # Configuração dos parâmetros de conexão
-server = 'CPP-077\SQLEXPRESS'  # substitua 'localhost\SQLEXPRESS' pelo nome do servidor do seu SQL Server Express
+server = 'SQLEXPRESS'  # substitua 'localhost\SQLEXPRESS' pelo nome do servidor do seu SQL Server Express
 database = 'CARGACOMPLETA'  # substitua 'NomeDoSeuBancoDeDados' pelo nome do seu banco de dados
 username = 'sa'  # substitua 'seu_usuario' pelo nome de usuário do banco de dados
-password = '22062011'  # substitua 'sua_senha' pela senha do banco de dados
+password = '******'  # substitua 'sua_senha' pela senha do banco de dados
 
 # String de conexão
 conn_str = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
